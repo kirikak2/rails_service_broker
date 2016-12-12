49b80394c15de26fb1479a4e1f3d757f0e9cfec7
Toshio Maki <toshio.maki.yy@hitachi-solutions.com>
2016-12-12 19:37:34 +0900

    Fix return parameter

--- lib/generators/templates/apis/v2/service_instance.rb
+++ lib/generators/templates/apis/v2/service_instance.rb
@@ -8,6 +8,10 @@ module <%= plural_name.singularize.classify %>
         # 200 OK The expected response body is below.
         put '/:instance_id' do
           # TODO Write code to return HTTP status code for create service instance
+          return {
+            dashboard_url: "http://path.to/dashboard_url",
+            operation: "create in progress" # TODO 'operation' field is optional.
+          }
         end
 
         desc 'GET /<%= plural_name.singularize %>/v2/service_instances/:instance_id/last_opration'
@@ -16,6 +20,9 @@ module <%= plural_name.singularize.classify %>
         # 410 GONE Appropriate only for asynchronous delete requests.
         get '/:instance_id/last_operation' do
           # TODO Write code to return HTTP status code for create service progress(optional)
+          return {
+            operation: "create in progress" # TODO 'operation' field is optional
+          }
         end
 
         desc 'PATCH /<%= plural_name.singularize %>/v2/service_instances/:instance_id'
@@ -25,6 +32,9 @@ module <%= plural_name.singularize.classify %>
         # 422 Unprocessable entity May be returned if the particular plan change requested is not supported.
         patch '/:instance_id' do
           # TODO Write code to return HTTP status code for update service instance(optional)
+          return {
+            operation: "update in progress" # TODO 'operation' field is optional
+          }
         end
 
         desc 'DELETE /<%= plural_name.singularize %>/v2/service_instances/:instance_id'
@@ -35,6 +45,9 @@ module <%= plural_name.singularize.classify %>
         # 422 Unprocessable Entity
         delete '/:instance_id' do
           # TODO Write code to return HTTP status code for delete service instance
+          return {
+            operation: "delete in progress" # TODO 'operation' field is optional
+          }
         end
       end
     end
