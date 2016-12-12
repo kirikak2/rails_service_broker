32a740367e15391186fd83b7173de336d2e60693
Toshio Maki <toshio.maki.yy@hitachi-solutions.com>
2016-12-12 19:00:11 +0900

    add context root to description

--- lib/generators/templates/apis/v2/service_binding.rb
+++ lib/generators/templates/apis/v2/service_binding.rb
@@ -4,7 +4,7 @@ module <%= plural_name.singularize.classify %>
       resource :service_instances do
         route_param :instance_id do
           resource :service_bindings do
-            desc 'PUT /v2/service_instances/:instance_id/service_bindings/:binding_id'
+            desc 'PUT /<%= plural_name.singularize %>/v2/service_instances/:instance_id/service_bindings/:binding_id'
             # Bind service
             # 201 Created Binding has been created.
             # 200 OK the binding already exists and requested parameters are identical to the existing binding.
@@ -14,7 +14,7 @@ module <%= plural_name.singularize.classify %>
               # TODO Write code to return HTTP status code for create service binding result.
             end
 
-            desc 'DELETE /v2/service_instances/:instance_id/service_bindings/:binding_id'
+            desc 'DELETE /<%= plural_name.singularize %>/v2/service_instances/:instance_id/service_bindings/:binding_id'
             # Unbinding service
             # 200 OK Binding was deleted.
             # 410 Gone Binding does not exist.
--- lib/generators/templates/apis/v2/service_instance.rb
+++ lib/generators/templates/apis/v2/service_instance.rb
@@ -2,7 +2,7 @@ module <%= plural_name.singularize.classify %>
   module V2
     class ServiceInstance < Grape::API
       resource :service_instances do
-        desc 'PUT /v2/service_instances/:instance_id'
+        desc 'PUT /<%= plural_name.singularize %>/v2/service_instances/:instance_id'
         # Catalog Management
         # Status Code
         # 200 OK The expected response body is below.
@@ -10,7 +10,7 @@ module <%= plural_name.singularize.classify %>
           # TODO Write code to return HTTP status code for create service instance
         end
 
-        desc 'GET /v2/service_instances/:instance_id/last_opration'
+        desc 'GET /<%= plural_name.singularize %>/v2/service_instances/:instance_id/last_opration'
         # Polling Last Operation(async only)
         # 200 OK The expected response body is below.
         # 410 GONE Appropriate only for asynchronous delete requests.
@@ -18,7 +18,7 @@ module <%= plural_name.singularize.classify %>
           # TODO Write code to return HTTP status code for create service progress(optional)
         end
 
-        desc 'PATCH /v2/service_instances/:instance_id'
+        desc 'PATCH /<%= plural_name.singularize %>/v2/service_instances/:instance_id'
         # Updating a Service Instance
         # 200 OK New plan is effective.
         # 202 Accepted Service instance update is in progress.
@@ -27,7 +27,7 @@ module <%= plural_name.singularize.classify %>
           # TODO Write code to return HTTP status code for update service instance(optional)
         end
 
-        desc 'DELETE /v2/service_instances/:instance_id'
+        desc 'DELETE /<%= plural_name.singularize %>/v2/service_instances/:instance_id'
         # Deprovisioning
         # 200 OK Service instance was deleted.
         # 202 Accepted Service instance deletion is in progress.
