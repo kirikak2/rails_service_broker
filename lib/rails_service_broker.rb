a30648ad690b7b2554659dea8aff06f667322743
Toshio Maki <kirika.k2@gmail.com>
2017-01-13 16:56:09 +0900

    add return code constants

--- lib/rails_service_broker.rb
+++ lib/rails_service_broker.rb
@@ -1,5 +1,6 @@
 require "rails_service_broker/version"
 require "rails_service_broker/engine"
+require "rails_service_broker/return_codes"
 
 require 'grape'
 require 'grape/jbuilder'
---
+++ lib/rails_service_broker/return_codes.rb
@@ -0,0 +1,8 @@
+module RailsServiceBroker
+  OK                   = 200
+  CREATED              = 201
+  ACCEPTED             = 202
+  CONFLICT             = 409
+  GONE                 = 410
+  UNPROCESSABLE_ENTITY = 422
+end
